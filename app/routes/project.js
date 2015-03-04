4371ba020b846ca8d32384f5e83c977b4f2e902a
Ben Babics <bbabics@gmail.com>
2015-03-04 09:51:52 -0500

    favorite button and action.

--- app/routes/project.js
+++ app/routes/project.js
@@ -4,6 +4,16 @@ export default Ember.Route.extend({
 
   model: function(params) {
     return this.store.find( 'project', params.project_id );
-  }
+  },
+
+  actions: {
+    'handleUnfavorite': function(model) {
+      console.log('handleUnfavorite', model);
+    },
+
+    'handleFavorite': function(model) {
+      console.log('handleFavorite', model);
+    },
+  },
 
 });
