f7adbeabd1e095bec139097d132649ac077036f0
Ben Babics <bbabics@gmail.com>
2015-03-31 16:55:51 -0400

    jshint cleanup

--- app/helpers/reminder-message.js
+++ app/helpers/reminder-message.js
@@ -1,18 +1,18 @@
 import Ember from 'ember';
 
 export function reminderMessage(input) {
-  var date = moment( input ),
-      diff = date.diff( moment(), 'days' ); // 1, future; 0, present; -1, past
+  var date = window.moment( input ),
+      diff = date.diff( window.moment(), 'days' ); // 1, future; 0, present; -1, past
 
   if ( !input ) {
-    return 'A reminder has not been set.'
+    return 'A reminder has not been set.';
   }
 
   switch( diff ) {
-    case -1: return 'Was reminded on: ' + date.calendar(); break;
-    case  0: return 'Remind me: '       + date.calendar(); break;
+    case -1: return 'Was reminded on: ' + date.calendar();
+    case  0: return 'Remind me: '       + date.calendar();
     default: return 'Remind me on: '    + date.calendar();
   }
-};
+}
 
 export default Ember.Handlebars.makeBoundHelper(reminderMessage);
