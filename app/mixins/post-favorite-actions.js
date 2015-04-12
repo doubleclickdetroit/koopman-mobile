dec26aa6153eeacf6f20b14f7497be45167fab79
Ben Babics <bbabics@gmail.com>
2015-04-12 15:14:03 -0400

    Stubbed initial "Login to use this feature" code.

--- app/mixins/post-favorite-actions.js
+++ app/mixins/post-favorite-actions.js
@@ -3,8 +3,20 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   actions: {
 
+    delegateAuthenticatedAction: function() {
+      var isAuthed = this.get( 'session.isAuthenticated' );
+      if ( !isAuthed ) {
+        alert( 'You must first sign-in to use this feature.' );
+      }
+      return isAuthed;
+    },
+
     handleFavoriteToggle: function(model) {
       var favorite;
+
+      if ( !this.send('delegateAuthenticatedAction') ) {
+        return false;
+      }
 
       if ( model.get( 'favorite' ) ) {
         model.get( 'favorite' ).destroyRecord();
--- app/routes/application.js
+++ app/routes/application.js
@@ -3,8 +3,12 @@ import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend( ApplicationRouteMixin, {
   model: function() {
+    function handleFailFn() {
+      return [];
+    }
+
     return Ember.RSVP.hash({
-      favorites: this.store.find( 'favorite' ),
+      favorites: this.store.find( 'favorite' ).catch( handleFailFn ),
       entries  : this.store.find( 'entry' ),
       projects : this.store.find( 'project' )
     });
