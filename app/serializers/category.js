25c2c3a57fcb218d1714b5e0391db26029cfa625
Ben Babics <bbabics@gmail.com>
2015-04-16 08:37:25 -0400

    Category cleanup

--- app/serializers/category.js
+++ app/serializers/category.js
@@ -1,12 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
-  primaryKey: 'ID',
-
-  extractArray: function(store, type, payload) {
-    payload = {
-      categories: payload
-    };
-    return this._super( store, type, payload );
-  }
+  primaryKey: 'ID'
 });
