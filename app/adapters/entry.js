1cf55b9a1854c87affe7f5eaff83be6c469df33f
Ben Babics <bbabics@gmail.com>
2015-05-24 12:50:12 -0400

    Updated BuildURL for Entry and Project for max posts

--- app/adapters/entry.js
+++ app/adapters/entry.js
@@ -3,7 +3,7 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   buildURL: function(type, id, record) {
-    var query = '?type[]=almanac_entry';
+    var query = '?type[]=almanac_entry&filter[order]=DESC&filter[orderby]=date&filter[posts_per_page]=100';
     return this._super( type, id, record ) + query;
   },
 
--- app/adapters/project.js
+++ app/adapters/project.js
@@ -3,7 +3,7 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   buildURL: function(type, id, record) {
-    var query = '?filter[category_name]=project-book-2';
+    var query = '?filter[category_name]=project-book-2&filter[order]=DESC&filter[orderby]=date&filter[posts_per_page]=100';
     return this._super( type, id, record ) + query;
   },
 
