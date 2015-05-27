16f9343a5acdd458e6ca0884c570a7d7b2abae92
Ben Babics <bbabics@gmail.com>
2015-05-27 15:59:39 -0400

    close pikabu also when link is clicked in sidebar

--- app/views/application.js
+++ app/views/application.js
@@ -8,10 +8,22 @@ export default Ember.View.extend({
   },
 
   currentPathDidChange: function() {
-    this.getPikabuInstance().closeSidebars();
+    this.closeSidebars();
   }.observes( 'controller.currentPath' ),
 
+  closeSidebars: function() {
+    this.getPikabuInstance().closeSidebars();
+  },
+
   didInsertElement: function() {
+    // init pikabu
     this.getPikabuInstance();
+
+    // in addition to changes in currentPath
+    // if a link is clicked in the sidebar, close it
+    this.$( '.m-pikabu-sidebar' ).on('click', 'a', function() {
+      this.closeSidebars();
+    }.bind( this ));
+
   }
 });
