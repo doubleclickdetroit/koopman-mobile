f3df93ec3e4f28e7751314585e9f04fb8f0223ed
Ben Babics <bbabics@gmail.com>
2015-05-29 18:34:27 -0400

    Reopen LinkView

---
+++ app/initializers/reopen-ember-link-view.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+
+var _hasRun = false;
+
+export function initialize() {
+  if ( _hasRun ) {
+    return;
+  }
+
+  Ember.LinkView.reopen({
+    attributeBindings: [ 'data-toggle', 'data-dismiss' ]
+  });
+};
+
+export default {
+  name: 'reopen-ember-link-view',
+  initialize: initialize
+};
