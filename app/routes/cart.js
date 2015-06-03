6cb6507dee9bcfc6baf7ba08b034bd977ee8218b
Ben Babics <bbabics@gmail.com>
2015-06-02 21:37:34 -0400

    Shopping List has Index back button

--- app/routes/cart.js
+++ app/routes/cart.js
@@ -5,7 +5,7 @@ export default Ember.Route.extend(ToolProductActionsMixin, {
   activate: function() {
     this.controllerFor( 'application' ).setProperties({
       'model.title'    : 'Shopping List',
-      'model.routeName': null
+      'model.routeName': 'index'
     });
   },
   deactivate: function() {
