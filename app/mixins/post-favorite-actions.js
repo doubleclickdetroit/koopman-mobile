55e18b6eb7173c80c3451308cdeb8c19d076d302
Ben Babics <bbabics@gmail.com>
2015-06-19 16:53:43 -0400

    Ensure user has session to render the shopping list

--- app/mixins/post-favorite-actions.js
+++ app/mixins/post-favorite-actions.js
@@ -11,6 +11,14 @@ export default Ember.Mixin.create({
 
 
   actions: {
+    handleDisplayShoppingList: function(model) {
+      if ( !this.delegateAuthenticatedAction() ) {
+        return false;
+      }
+
+      this.send( 'showModal', 'modal-shopping-list', model );
+    },
+
     handleFavoriteToggle: function(model) {
       var favorite;
 
