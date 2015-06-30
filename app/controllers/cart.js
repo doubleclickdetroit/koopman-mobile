b9fcf8722e6e7b97324bac81beaadfd3699b6583
Ben Babics <bbabics@gmail.com>
2015-06-29 20:36:13 -0400

    Shopping List accepts custom product entries

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   productCollection        : Ember.computed.filterBy( 'model.products', 'isHidden', false ),
   acquiredProductCollection: Ember.computed.filterBy( 'productCollection', 'hasAcquired', true ),
-  withoutAcquiredProducts  : Ember.computed.equal( 'acquiredProductCollection.length', 0 )
+  withoutAcquiredProducts  : Ember.computed.equal( 'acquiredProductCollection.length', 0 ),
+
+  customProduct: Ember.Object.create({
+    title: ''
+  })
 });
