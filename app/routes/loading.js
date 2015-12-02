01e6c4ebdf6dc74352a8d19fb8e8aa0d2eab80dc
Ben Babics <bbabics@gmail.com>
2015-12-02 13:56:30 -0500

    changed loading animation for ios

---
+++ app/routes/loading.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  activate: function() {
+    this.controllerFor( 'application' ).setProperties({
+      'model.title'    : null,
+      'model.routeName': null
+    });
+  }
+});
