8146ee77f6ceca992be21fb6c3ab25958622753f
Ben Babics <bbabics@gmail.com>
2016-02-03 10:16:48 -0500

    Entries/Entry proper scrolling for current month & year

---
+++ app/components/almanac-header.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'h4',
+  classNames: [ 'timeline-month' ],
+
+  yearAndMonthDidChange: function() {
+    var date   = new Date();
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+    var selectedYear = this.get( 'year' );
+    var currentYear  = ( new Date() ).getFullYear();
+
+    var month        = this.get( 'month' );
+    var currentMonth = months[ (new Date()).getMonth() ];
+
+    var isCurrentYear  = selectedYear == currentYear;
+    var isCurrentMonth = month == currentMonth;;
+
+    if ( isCurrentYear && isCurrentMonth ) {
+      Ember.run.schedule('afterRender', () => {
+        var $el = Ember.$( this.$() );
+        var height = Ember.$( '.header-elements' ).outerHeight();
+
+        Ember.$('#content').scrollTop(0).animate({
+          scrollTop: $el.offset().top - 60
+        }, 500, 'easeInOutQuad');
+      });
+    }
+  }.observes( 'year', 'month' ).on( 'init' )
+});
--- app/views/entry.js
+++ app/views/entry.js
@@ -2,4 +2,9 @@ import Ember from 'ember';
 import PostContentMixin from '../mixins/post-content-mixin';
 
 export default Ember.View.extend(PostContentMixin, {
+  didInsertElement() {
+    Ember.$('#content').animate({
+      scrollTop: 0
+    }, 500, 'easeInOutQuad');
+  }
 });
